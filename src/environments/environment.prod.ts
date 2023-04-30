7dfb8ac4fd379762f39eff629d2379d7d00b616d
Daniel Mills <danielm88341@gmail.com>
2023-04-30 14:20:23 -0400

    startup additions

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'loldanworkboarddemo',
+    appId: '1:639647735208:web:404e25ad4ad6bcfb76519d',
+    storageBucket: 'loldanworkboarddemo.appspot.com',
+    apiKey: 'AIzaSyCz97KJUulwF7DrnYUJD92plfpuhnzFJ7I',
+    authDomain: 'loldanworkboarddemo.firebaseapp.com',
+    messagingSenderId: '639647735208',
+    measurementId: 'G-SZ0C4CVT9D',
+  },
+  useEmulators: false,
   production: true
 };
