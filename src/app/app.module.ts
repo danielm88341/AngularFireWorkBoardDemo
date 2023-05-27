06345a9d0704c07cc2201fd9c2f85de06aa1efcb
Daniel Mills <danielm88341@gmail.com>
2023-05-27 12:30:51 -0400

    Meaningless drivel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {
   initializeFirestore,
   connectFirestoreEmulator
 } from '@angular/fire/firestore';
+import {SharedModule} from "./shared/shared.module";
 
 @NgModule({
   declarations: [
@@ -47,7 +48,8 @@ import {
       }
 
       return firestore;
-    })
+    }),
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
